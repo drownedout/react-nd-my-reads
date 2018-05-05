5c6547c46425fe236d2046cd33127173b29e3d79
drownedout <danielcdonnelly@gmail.com>
2018-05-05 14:21:12 -0400

    Search component

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import BookList from './BookList';
+import * as BooksAPI from './BooksAPI';
 
 class Search extends Component {
+
+  constructor(props){
+    super(props)
+    this.searchBooks = this.searchBooks.bind(this)
+    this.onShelfUpdate = this.onShelfUpdate.bind(this)
+  }
+
+  state = {
+    searchedBooks: [],
+  }
+
+  searchBooks = (query) => {
+    if (query===''){
+      this.setState({searchedBooks:[]})
+    }
+    BooksAPI.search(query.trim(), 3).then(books => {
+      if(books===undefined) {
+        this.setState({searchedBooks:[]})
+      }
+      this.setState({searchedBooks: books})
+    })
+  }
+
+  onShelfUpdate(book, newShelf){
+    this.props.onShelfUpdate(book, newShelf)
+  }
+
   render(){
+    const { searchedBooks } = this.state;
+    const { books } = this.props;
     return(
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
             {/*
             NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -15,11 +47,20 @@ class Search extends Component {
             However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
             you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={(event) => this.searchBooks(event.target.value)}/>
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            <BookList 
+              name='Search Results'
+              books={searchedBooks} 
+              onShelfUpdate = {this.onShelfUpdate}
+              />
+          </ol>
         </div>
       </div>
     )
